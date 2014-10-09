98d891616f6d5c12ff06cc565501cdb2c319eb44
Jack McDermott <jackrmcdermott@gmail.com>
2014-10-08 22:01:17 -0400

    added wistia video

--- scripts/script.js
+++ scripts/script.js
@@ -1,66 +1,25 @@
 
 $(document).ready(function() {
-	
-	
 // Navigation SlideDown/Up on Mobile
-	
 	$('#menu').click(function() {
-		
 		$('header ul').slideToggle(300);
-			
 	});
-
-				
 // Tweet to Download eBook
-	
     $('#tweetLink').tweetAction({
         text:		'Learning about social media for #slpeeps',
         url:		'http://appstore.com/speech4good',
         via:		'speech4good',
         related:	'speech4good'
     },function() {
-  
         // Callback function. Triggered when the user closes the pop-up window:
-   
         $('a.downloadButton')
                 .addClass('active')
                 .attr('href','ebook.pdf');
-                
         $('.tweet_btn').addClass('inactive');
-                
     });
 
-        
-// Quote Slider
-
-document.getElementById("left").onclick=function() {
-	
-	
-	
-	
-}
-	
-     
-// Re-order feature gif for mobile
-
-/*
-$(window).bind("load resize", listenWidth);
-			
-	function listenWidth() {
-				
-		if($(window).width() < 900) {
-					
-			$(".image-dos").remove().insertBefore($(".text-dos"));
-				
-		}
-				
-		else {
-					
-			$(".image-dos").remove().insertAfter($(".text-dos"));
-						
-		}
-	}
-*/
-	
-}); //end document.ready	
-	
+//Stuttering Awareness
+   $(document).ready(function () {
+  $("#isad-main-content").delay("100").css('opacity',0).animate({opacity:1}, 1000);
+}); //end document.ready
+}); //end document.ready
